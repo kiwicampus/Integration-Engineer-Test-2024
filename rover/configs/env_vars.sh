f8ec8b23bc85f9656f623720960e178c000b4054
garzonwilmer5 <garzonwilmer5@gmail.com>
2024-11-21 12:18:47 -0500

    [FEAT] Fix midleware

--- rover/configs/env_vars.sh
+++ rover/configs/env_vars.sh
@@ -1,4 +1,5 @@
-# -----------------------------------------------------------------------------
 # CONSTANTS
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp # [string] to choose between dds implementations, choose between rmw_fastrtps_cpp / rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=/workspace/rover/configs/cyclone_configuration.xml # [string] Select the Network Interface Address
 
-export AMAZING_ENV_VAR="you are amazing"
+
